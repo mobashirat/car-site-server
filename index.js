1bac5d9497e347d235dd595144935fb7c319affa
mobashirat <mirzatazneen@gmail.com>
2021-11-29 23:35:23 +0600

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require("mongodb").ObjectId;
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 
 app.use(cors())
